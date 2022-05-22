84f5f4de9f400f2f3382f53b58d43ea97f4e6c7e
NumiBlue <thekindlyblog@gmail.com>
2022-05-22 10:26:27 -0400

    fresh attempt

--- quizlet.js
+++ quizlet.js
@@ -77,40 +77,42 @@ const myQuestions= [
   myQ0= {
   question: "Rumi was ______.",
  options: ["a Roman Emperor", "a Sufi mystic and poet", "a Taoist monk", "an American beat poet"],
- answer: "2"
+ answer: "1"
 },
 //1
 myQ1= {
   question: "What book did St. Theresa of Avila write",
   options: ["The Interior Castle", "Dark Night of the Soul", "Conversations with God", "The Necronomicon"],
-  answer: "1"
+  answer: "0"
 },
 //2
 myQ2= {
   question: "Mirabai was a devotee of which god?",
   options: ["Zeus", "Durga", "Sri Krishna", "Siddhartha Gautama"],
-  answer: "3"
+  answer: "2"
 },
 //3
 myQ3= {
   question: "What was the Buddha's original name?",
   options: ["Siddhartha Gautama", "His Holiness, the Dalai Lama", "St. Patrick", "Gupta"],
-  answer: "1"
+  answer: "0"
 },
 //4
 myQ4= {
   question: "St. Phoebe is mentioned in the Bible as a ______?",
    options: ["Prophetess", "Martyr", "Wife of an Apostle", "Deacon"],
-   answer: "4"
+   answer: "3"
 },
 //5
 myQ5= {
   question: "St. Hildegard of Bingen described the influence of God as what?",
   options: ["The greening", "the darkening", "the shining", "the awakening"],
-  answer: "1"
+  answer: "0"
 }
 ]
-//Stringify and aStore answers
+//For each question, grab question div and each input
+//Take array answers, display in inputs, and display question to 42, store as array (array methods)
+//Stringify and Store answers
 const answers = { 'myQ0': 2, 'myQ1': 1, 'myQ2': 3, 'myQ3':1, 'myQ4': 4, 'myQ5': 1};
 //Put answers in local storage
 localStorage.setItem('answers', JSON.stringify, (answers));
@@ -127,3 +129,11 @@ document.getElementById("start-btn").addEventListener("click", function () {
   //displays as array
  // displayInfo()
 }) 
+
+//Attempt
+let question_display = document.getElementById("ques");
+let answer_display1 = document.getElementById("1");
+let answer_display2 = document.getElementById("2");
+let answer_display3 = document.getElementById("3");
+let answer_display4 = document.getElementById("4");
+
