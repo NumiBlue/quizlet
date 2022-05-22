34ac11adb798e901d36d9c254f04fb6f05b3b616
NumiBlue <thekindlyblog@gmail.com>
2022-05-22 11:45:18 -0400

    restructures questions and answers in array

--- script.js
+++ script.js
@@ -74,41 +74,77 @@ const rules = document.querySelector("#rules")
 //Questions
 const myQuestions= [
   //0
-  myQ0= {
+ {
+   myQ: 0,
   question: "Rumi was ______.",
- options: ["a Roman Emperor", "a Sufi mystic and poet", "a Taoist monk", "an American beat poet"],
+ options: [
+   "a Roman Emperor",
+  "a Sufi mystic and poet", 
+  "a Taoist monk", 
+  "an American beat poet"
+],
  answer: "1"
 },
 //1
-myQ1= {
+{
+  myQ: 1,
   question: "What book did St. Theresa of Avila write",
-  options: ["The Interior Castle", "Dark Night of the Soul", "Conversations with God", "The Necronomicon"],
+  options: [
+    "The Interior Castle", 
+  "Dark Night of the Soul", 
+  "Conversations with God", 
+  "The Necronomicon"
+],
   answer: "0"
 },
 //2
-myQ2= {
+{
+  myQ: 2,
   question: "Mirabai was a devotee of which god?",
-  options: ["Zeus", "Durga", "Sri Krishna", "Siddhartha Gautama"],
+  options: [
+    "Zeus", 
+  "Durga", 
+  "Sri Krishna", 
+  "Siddhartha Gautama"
+],
   answer: "2"
 },
 //3
-myQ3= {
+{
+  myQ: 3,
   question: "What was the Buddha's original name?",
-  options: ["Siddhartha Gautama", "His Holiness, the Dalai Lama", "St. Patrick", "Gupta"],
+  options: [
+    "Siddhartha Gautama", 
+  "His Holiness, the Dalai Lama", 
+  "St. Patrick", 
+  "Gupta"
+],
   answer: "0"
 },
 //4
-myQ4= {
+{
+  myQ: 4,
   question: "St. Phoebe is mentioned in the Bible as a ______?",
-   options: ["Prophetess", "Martyr", "Wife of an Apostle", "Deacon"],
+   options: [
+     "Prophetess", 
+     "Martyr", 
+     "Wife of an Apostle", 
+     "Deacon"
+    ],
    answer: "3"
 },
 //5
-myQ5= {
+{
+  myQ: 5,
   question: "St. Hildegard of Bingen described the influence of God as what?",
-  options: ["The greening", "the darkening", "the shining", "the awakening"],
+  options: [
+    "The greening", 
+    "the darkening", 
+    "the shining", 
+    "the awakening"
+  ],
   answer: "0"
-}
+},
 ]
 //For each question, grab question div and each input
 //Take array answers, display in inputs, and display question to 42, store as array (array methods)
