e71b0ae5657bcfe9ac41e147260c1f1ef229cc47
NumiBlue <thekindlyblog@gmail.com>
2022-06-07 15:26:13 -0400

    saves high score elements

--- script.js
+++ script.js
@@ -193,4 +193,31 @@ initialsEl.onkeyup = checkForEnter;
   }
 
   //highscores
+  function showScores() {
+    
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+   highscores.sort(function(a, b) {
+      return b.score - a.score;
+    });
+  
+    highscores.forEach(function(score) {
+      // create li tag high score
+      var liTag = document.createElement("li");
+      liTag.textContent = score.initials + " - " + score.score;
+  
+      // display on page
+      var whichEl = document.getElementById("highscores");
+      whichEl.appendChild(liTag);
+    });
+  }
+  
+  function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+  }
+  
+  document.getElementById("clear").onclick = clearHighscores;
+  
+  // run function when page loads
+  showScores();
   
